51eb419ddf77ad8c81ed119209e4c7dea2bb73d0
raghav-vish <raghav.vish@gmail.com>
2022-03-05 17:42:57 +0530

    Added a few lines missed out in first commit

--- monochrome.py
+++ monochrome.py
@@ -4,10 +4,13 @@ import time
 
 pyautogui.PAUSE=0.001 #Lower the number, faster the program will run
 
-img=Image.open("cs.jpg") # Replace with the path of the image you want painted
+img=Image.open("blah.jpg") # Replace with the path of the image you want painted
 img=ImageOps.scale(img, factor=0.5) #To scale down huge images
+grscl_img=ImageOps.grayscale(img)
+grscl_pixels=grscl_img.load()
 pixels = img.load()
-
+img.show()
+sys.exit(0)
 
 '''Defines starting location.
 startx and starty can be changed to start painting from another place.
